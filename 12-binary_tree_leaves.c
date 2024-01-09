2a1c811b902eb6243fa2a7775159d0f9e4d4aafe
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-09 10:45:52 +0100

    added comments in 12-binary_tree_leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: 0 if tree is null, otherwise the number of leaves
+ */
+
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t size = 0;
