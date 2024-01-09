ad4a4b180b2c40969e2deaec70f91f1f794e2168
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-09 13:28:34 +0100

    added comment to 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,12 @@
 #include "binary_trees.h"
 
-
+/**
+ * check_if_leaves_at_same_level - check if all leaves are at same level
+ * @tree: pointer to the node in the tree to check
+ * @level: level where the node is
+ * @depth: the depth at which the leaves should be
+ * Return: 1 (true) if the leaf is at the right depth, 0 otherwise (false);
+ */
 int check_if_leaves_at_same_level(const binary_tree_t *tree, int level,int depth)
 {
 	if (tree == NULL)
@@ -11,6 +17,11 @@ int check_if_leaves_at_same_level(const binary_tree_t *tree, int level,int depth
 	check_if_leaves_at_same_level(tree->right, level + 1, depth);
 }
 
+/**
+ * binary_tree_is_perfect - check if the binary tree is perfect
+ * @tree: pointer to the root of the tree
+ * Return: 1 if perfect, 0 otherwise.
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int isfull, depth, leaves_same_level;
