92546d4e29f491480d3e467fffee0b437370c1e2
christophemr <christophemr59@gmail.com>
2024-01-09 12:25:35 +0100

    removal of whitespace(betty)

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-/*
+/**
  * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
  * @tree: pointer to the root node of the tree to count the number of nodes
  * Return: If tree is NULL, the function must return 0, else node count
@@ -20,7 +20,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		/*in the left subtree*/
 		count += binary_tree_nodes(tree->right);
 		/*recursively counts nodes with at least 1 child*/
-                /*in the right subtree*/
+		/*in the right subtree*/
 		return (count);
 		/* total number of nodes*/
 	}
