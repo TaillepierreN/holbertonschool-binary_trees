825621d8e80047d2ef19c39a336585bca93fc521
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-10 09:27:19 +0100

    added is_full and is_full_recursive into 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,44 @@
 #include "binary_trees.h"
+/**
+ * is_full_recursive - checks if a binary tree is full resursively
+ * @tree: pointer to the root node of the tree to check
+ * Return: 0 if not full, 1 if full
+ */
+int is_full_recursive(const binary_tree_t *tree)
+{
+	if (tree != NULL)
+	/*if current node is not empty*/
+	{
+		if ((tree->left != NULL && tree->right == NULL) ||
+		(tree->left == NULL && tree->right != NULL))
+		/*check if current node has exactly one child(not full)*/
+		return (0);
+		/*recursive call for each childs*/
+		/* if any of them if not full, the tree is not full*/
+		if (is_full_recursive(tree->left) == 0 ||
+			is_full_recursive(tree->right) == 0)
+			return (0);
+			/*tree not full*/
+	}
+	return (1);
+	/*if current node is NULL or subtrees are full*/
 
+}
+
+/**
+ * is_full - checks if a binary tree is full
+ * @tree: pointer to the root node of the tree to check
+ * Return: 0 if tree is not NULL or not full, 1 otherwise
+ */
+
+int is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	/* check if tree is empty*/
+		return (0);
+	/*call recursive function to check if the tree is full*/
+	return (is_full_recursive(tree));
+}
 /**
  * check_if_leaves_at_same_level - check if all leaves are at same level
  * @tree: pointer to the node in the tree to check
@@ -29,7 +68,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	isfull = binary_tree_is_full(tree);
+	isfull = is_full(tree);
 	if (!isfull)
 		return (0);
 	depth = binary_tree_height(tree);
