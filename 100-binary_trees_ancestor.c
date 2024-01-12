755542d78f06845489de0ab8c3b4a8e44c01a1a3
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-12 10:15:08 +0100

    Added advanced task 19-20-21

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+ * @first: pointer to first node
+ * @second: pointer to second node
+ *
+ * Return: Pointer to the lowest common ancestor node of the two nodes
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	binary_tree_t *parent_first, *parent_second;
+
+	/* Check if node is NULL*/
+	if (first == NULL || second == NULL)
+		return (NULL);
+	/*if nodes are the same, they are the lowest common ancestor*/
+	if (first == second)
+		return (first);
+	/*get the parents of each node*/
+	parent_first = first->parent;
+	parent_second = second->parent;
+
+	/*Check if the first node is parent of the second
+	or if the first is at root level.*/
+	/*If true, start searching for the ancestor between the
+	first node and the parent of the second node.*/
+	if (first = parent_second || parent_first == NULL
+	|| (!parent_first->parent && parent_second))
+		return (binary_trees_ancestor(first, parent_second));
+	/*Check if the second node is a parent of the first 
+	or if the second is at root level.*/
+    /*If true, start searching for the ancestor between the
+	parent of the first node and the second node.*/
+	if (second = parent_first || parent_second == NULL
+	|| (!parent_second->parent && parent_first))
+		return (binary_trees_ancestor(parent_first, second));
+
+	/*use recursion to find the ancestor one level above for both nodes*/
+	return (binary_trees_ancestor(parent_first, parent_second));
+}
---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,51 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_levelorder - Goes through a binary tree using level-order traversal
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	binary_tree_t **queue;
+	int front = 0, rear = 0, size = 0;
+
+	if (tree == NULL || func == NULL)
+		return;
+
+	// Initial size of the queue
+	size = 10;
+	queue = malloc(sizeof(*queue) * size);
+	if (queue == NULL)
+		return;
+
+	// queue the root
+	queue[rear++] = (binary_tree_t *)tree;
+
+	while (front < rear)
+	{
+		// remove from queue a node from the queue and process it
+		binary_tree_t *temp = queue[front++];
+		func(temp->n);
+
+		// Resize the queue if necessary
+		if (rear >= size)
+		{
+			size *= 2;
+			queue = realloc(queue, sizeof(*queue) * size);
+			if (queue == NULL)
+				return;
+		}
+
+		// queue the left child
+		if (temp->left != NULL)
+			queue[rear++] = temp->left;
+
+		// queue the right child
+		if (temp->right != NULL)
+			queue[rear++] = temp->right;
+	}
+
+	// Clean up
+	free(queue);
+}
